385efeda806a27f72e3eed396722967e84c4f7f7
Daniel Duy Mai <70843762+DanielDuy@users.noreply.github.com>
2023-10-10 15:47:29 +0200

    Update TestTeque.java

--- TestTeque.java
+++ TestTeque.java
@@ -2,17 +2,17 @@ public class TestTeque {
     public static void main(String[] args) {
         Teque t = new Teque();
 
-        t.push_back(3);
         t.push_back(7);
-        t.push_back(9);
+        t.push_back(15);
+        t.push_back(22);
 
-        t.push_front(2);
+        t.push_front(5);
         t.push_front(1);
 
-        t.push_middle(5);
-        t.push_middle(4);
+        t.push_middle(12);
+        t.push_middle(9);
 
-        System.out.println("Queue:");
+        System.out.println("-Queue-");
         t.printQueue();
         System.out.println("-End of queue-");
 
@@ -21,7 +21,7 @@ public class TestTeque {
         System.out.println("\nHenter fra første element i queue-en:");
         System.out.println(t.hent()+"\n");
 
-        System.out.println("Queue:");
+        System.out.println("-Queue-");
         t.printQueue();
         System.out.println("-End of queue-");
     }
